7fc6311479166d2f1c4a107a3136e7178809bcbb
Nikolai Hovis <nikolai.hovis@gmail.com>
2026-08-22 07:27:16 -0500

    a LOT of updates

--- Core.cs
+++ Core.cs
@@ -1,17 +1,69 @@
 ﻿using Alta;
+using Alta.Blacksmithing;
+using Alta.Chunks;
 using Alta.Inventory;
 using Alta.Networking;
 using HarmonyLib;
 using MelonLoader;
 using MelonLoader.Utils;
+using System.Diagnostics;
 using System.Reflection;
 using UnityEngine;
+using static SpawnHelper;
 
 [assembly: MelonInfo(typeof(CustomRecipesAPI.Core), "CustomRecipesAPI", "1.0.0", "CGNik", null)]
 [assembly: MelonGame("Alta", "A Township Tale")]
 
 namespace CustomRecipesAPI
 {
+    public class SpawnPatch
+    {
+        static MethodBase TargetMethod()
+        {
+            return AccessTools.Method(
+                typeof(SpawnHelper),
+                "Spawn",
+                new Type[]
+                {
+                    typeof(NetworkPrefab),
+                    typeof(SpawnData),
+                    typeof(Chunk),
+                    typeof(Vector3),
+                    typeof(Quaternion),
+                    typeof(SpawnHelper.SpawningCallback)
+                }
+            );
+        }
+
+        internal static bool Prefix(NetworkPrefab prefab, SpawnData spawnData, Chunk chunk, ref Vector3 position, ref Quaternion rotation, SpawningCallback preChunking)
+        {
+            StackTrace stackTrace = new StackTrace();
+            StackFrame[] stackFrames = stackTrace.GetFrames();
+            MethodBase method = stackFrames[2].GetMethod();
+            if (method == null) { return true; }
+            Type stateMachineType = method.DeclaringType;
+            if (stateMachineType == null) { return true; }
+            Type originalType = stateMachineType.DeclaringType;
+            if (originalType == null) { return true; }
+            if (originalType.Name != "Smelter") { MelonLogger.Msg($"not from Smelter, was from {originalType.Name}"); return true; }
+            if (stateMachineType.Name.Substring(0,13) != "<TrySmelt>d__") { MelonLogger.Msg($"not in TrySmelt, substring was {stateMachineType.Name.Substring(0,13)}"); return true; }
+
+            if (Core.smelterSpawnPositionOffsets.ContainsKey(prefab.Hash))
+            {
+                Vector3 p = Core.smelterSpawnPositionOffsets[prefab.Hash];
+                float a = -rotation.eulerAngles.y * Mathf.Deg2Rad;
+                position += new Vector3(p.x * Mathf.Cos(a) - p.z * Mathf.Sin(a), p.y, p.z * Mathf.Cos(a) + p.x * Mathf.Sin(a));
+            }
+
+            if (Core.smelterSpawnRotationOffsets.ContainsKey(prefab.Hash))
+            {
+                rotation.eulerAngles += Core.smelterSpawnRotationOffsets[prefab.Hash];
+            }
+
+            return true;
+        }
+    }
+
     public class InitializePatch
     {
         internal static void Postfix(NetworkPrefab __instance)
@@ -40,10 +92,18 @@ namespace CustomRecipesAPI
         public static event Action PreSetUpMoulds = () => { };
         public static event Action SetUpMoulds = () => { };
         public static event Action PostSetUpMoulds = () => { };
-        public static event Action PostInitializePatch = () => { };
+        public static event Action PostPatches = () => { };
 
+        // Adding an Item to this will add it to the filter that the Standard Mould Press uses for the weapon to be turned into a Mould
+        // This allows you to use said Item to make Moulds with the Standard Mould Press
         public static List<Item> itemsToAddToStandardMouldPress = [];
+        // Same thing, but with the Hebios Mould Press
         public static List<Item> itemsToAddToHebiosMouldPress = [];
+        // Adding an Item's Prefab's Hash to this will cause the Smelter to spawn the Item with that Vector3 as a position offset when creating said Item with a Mould
+        // The purpose of this is to prevent certain items from getting stuck in the Smelter
+        public static Dictionary<uint, Vector3> smelterSpawnPositionOffsets = [];
+        // Same thing, but with rotation instead of position
+        public static Dictionary<uint, Vector3> smelterSpawnRotationOffsets = [];
 
         public override void OnInitializeMelon()
         {
@@ -59,8 +119,21 @@ namespace CustomRecipesAPI
             PostSetUpMoulds.Invoke();
 
             HarmonyInstance.Patch(AccessTools.Method(typeof(NetworkPrefab), "Initialize"), postfix: new HarmonyMethod(typeof(InitializePatch), nameof(InitializePatch.Postfix)));
+            HarmonyInstance.Patch(AccessTools.Method(
+                typeof(SpawnHelper),
+                "Spawn",
+                new Type[]
+                {
+                    typeof(NetworkPrefab),
+                    typeof(SpawnData),
+                    typeof(Chunk),
+                    typeof(Vector3),
+                    typeof(Quaternion),
+                    typeof(SpawnHelper.SpawningCallback)
+                }
+            ), prefix: new HarmonyMethod(typeof(SpawnPatch), nameof(SpawnPatch.Prefix)));
 
-            PostInitializePatch.Invoke();
+            PostPatches.Invoke();
         }
 
         public static void RegisterMouldDefinition(MouldDefinition mouldDefinition)
